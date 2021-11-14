c93f4404bec6d6f6a59d0925070cc9275f66ff9b
Brendan-Go <94251767+Brendan-Go@users.noreply.github.com>
2021-11-14 04:42:56 -0600

    Add files via upload

--- costData.java
+++ costData.java
@@ -3,13 +3,15 @@ import java.util.*;
 import java.io.*;
 public class costData {
 	 public String state;
-	    public double costLiving, food, transport, housing, misc, health;
-	    costData(String astate, double acost, double afood, double atransport, double ahousing, double amisc, double ahealth){
+	    public double costLiving, food, transport, housing, util, misc, health;
+	    costData(String astate, double acost, double afood, double atransport, double ahousing, double autil, double amisc, double ahealth){
 	        state = astate;
 	        costLiving = acost;
 	        food = afood;
 	        transport = atransport;
 	        housing = ahousing;
 	        misc = amisc;
+	        util = autil;
+	        health = ahealth;
 	    }
 }
--- driver.java
+++ driver.java
@@ -10,7 +10,7 @@ public class driver {
 		Scanner input = new Scanner(System.in);
 		costData[] col = new costData[50];
 		String state;
-		double util, med, food, others, housing;
+		double util, med, food, others, housing, transport;
 		col = no.parser();
 		
 		System.out.println("Financial Score Calculator");
@@ -27,8 +27,8 @@ public class driver {
 		 housing = input.nextDouble();
 		 System.out.print("On average how much do you spend on others per month? ");
 		 others = input.nextDouble();
-		 //System.out.print("what is " + state + "'s income tax for your tax bracket?");
-		 //incometax = input.nextDouble();
+		 System.out.print("On average how much do you spend on transport per month? ");
+		 transport = input.nextDouble();
 		 
 		 
 		//_________________________________________________________for loop
@@ -44,19 +44,16 @@ public class driver {
 		 //________________________________________________________
 		 double total = util + med + food + housing + others;
 		 
-		 
-		 
 		 System.out.println("Your average cost of living is $" + total + " compared to the average cost of living in " + state + " is $" + col[counter].costLiving);
 		 
+		 //utilizing score
+		 int tScore = score(util, col[counter].util) + score(med, col[counter].health) + score(food, col[counter].food) + score(housing, col[counter].housing) + score(others, col[counter].misc) 
+		  + score(transport, col[counter].transport);
+		 tScore /= 6;
 		 
-		 /* 
-		  arbitrary score from program
-		  utilAvg = 100 * (math.abs(util - state average));
-		  
-		  final score = (income + food + med + util)/4
-		  print finalscore;
-		  print reccommendations	
-		  */
+		 System.out.println("Your financial score is: " + tScore);
+		 
+		 
     }
 
 public costData[] parser() throws FileNotFoundException 
@@ -66,10 +63,35 @@ public costData[] parser() throws FileNotFoundException
     int index = 0;
     reader.useDelimiter("\t");
     while (reader.hasNext()){
-        list[index++] = new costData(reader.next(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble());
+        list[index++] = new costData(reader.next(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(),reader.nextDouble(), reader.nextDouble());
         reader.nextLine();
     }
     	return list; 
 	}
+public static int score(double stateNum, double base)
+{
+	double defic = 0.0;	//deficit
+	boolean lt = false;	//bool var for if the amount spent is under the minimum cost of living
+	int score = 0;	//return variable
+	
+	if(stateNum < base)	//address under cost of living case 
+	{
+		defic = (base - stateNum);
+		lt = true;
+	}
+	else if (stateNum >= base)	//normal case - above cost of living
+	{
+		defic = (stateNum - base);
+	}
+	
+	if(!lt)
+	{
+		score = (int) (100*(1 - (defic/base)));	//normal case - total score out of 100, take the % off from the base and multiply 100 by it - more points based off of how close you are to minimum cost of living
+	}
+	else
+		return -1;	//return -1 for less than, deal with this in driver - reccommend different financial advice links as it is likely the client is struggling 
+	return score;
+	
+}
 
 }
